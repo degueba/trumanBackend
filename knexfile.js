d907e2c2dae438ce2856a70b1a680f11a9b4909f
Rodrigo Botelho <rbotelho_prest@telecine.com.br>
2020-04-01 15:58:01 -0300

    fix some paths migrations

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 // Update with your config settings.
 const path = require('path');
 
-console.log(path.resolve(__dirname))
+console.log(path.resolve(__dirname, 'src/database/migrations'))
 module.exports = {
 
   development: {
@@ -10,7 +10,7 @@ module.exports = {
       filename: path.resolve(__dirname, 'db.sqlite')
     },
     migrations: {
-      directory: path.resolve(__dirname, '/src/database/migrations')
+      directory: path.resolve(__dirname, 'src/database/migrations')
     },
     useNullAsDefault: true
   },
