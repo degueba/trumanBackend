9a4022cd01b1243ee040f3c302d06d94fbd72a8c
Rodrigo Botelho <rbotelho_prest@telecine.com.br>
2020-04-01 12:45:58 -0300

    msg running server

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,4 +3,6 @@ const configuration = require('../../knexfile');
 
 const connection = knex(configuration.development);
 
+console.log('Running on http://localhost:3333')
+
 module.exports = connection;
